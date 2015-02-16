70820eca0a02595e49dea715bc476f6b6bd74668
Mir Md. Asif Hossain <asif.hossain@therapservices.net>
2015-02-16 11:48:05 +0600

    Challange Level 1 Section 3-1

--- js/App.js
+++ js/App.js
@@ -1,11 +1,17 @@
 
 (function () {
 
-    var gem = { name: 'Azurite', price: '20'};
     var app = angular.module('store',[]);
     app.controller('StoreController', function () {
         this.product = gem;
     });
+
+    var gem = {
+        name: 'Azurite',
+        price: '20',
+        canPurchase: false,
+        soldOut: true
+    };
 
 })();
 
