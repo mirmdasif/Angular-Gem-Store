220341278f62071c9fd3706c2c36758ac4ac93f9
Mir Md. Asif Hossain <asif.hossain@therapservices.net>
2015-02-22 13:22:26 +0600

    Level 3 challenge 1

--- js/App.js
+++ js/App.js
@@ -6,6 +6,15 @@
         this.products = gems;
     });
 
+    app.controller('ReviewFormController', function () {
+        this.review = {};
+        this.submitReview = function (product) {
+
+            product.reviews.push(this.review);
+            this.review = {};
+        }
+    })
+
     var gems =[
         {
             name: 'Azurite',
@@ -16,7 +25,18 @@
                 "images/gem-01.gif",
                 "images/gem-02.gif",
                 "images/gem-03.gif"
-            ]
+            ],
+            reviews: [{
+                stars: 5,
+                body: "I love this gem!",
+                author: "joe@example.org",
+                createdOn: 1397490980837
+            }, {
+                stars: 1,
+                body: "This gem sucks.",
+                author: "tim@example.org",
+                createdOn: 1397490980837
+            }]
         },
         {
             name: 'BloodStone',
@@ -27,7 +47,18 @@
                 "images/gem-04.gif",
                 "images/gem-05.gif",
                 "images/gem-06.gif"
-            ]
+            ],
+            reviews: [{
+                stars: 5,
+                body: "I love this gem!",
+                author: "joe@example.org",
+                createdOn: 1397490980837
+            }, {
+                stars: 1,
+                body: "This gem sucks.",
+                author: "tim@example.org",
+                createdOn: 1397490980837
+            }]
         },
         {
             name: 'Zircon',
@@ -38,7 +69,18 @@
                 "images/gem-07.gif",
                 "images/gem-08.gif",
                 "images/gem-09.gif"
-            ]
+            ],
+            reviews: [{
+                stars: 5,
+                body: "I love this gem!",
+                author: "joe@example.org",
+                createdOn: 1397490980837
+            }, {
+                stars: 1,
+                body: "This gem sucks.",
+                author: "tim@example.org",
+                createdOn: 1397490980837
+            }]
         }
     ];
 
