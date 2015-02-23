6fb6cd427be4a05944eb4af1bb401a2506f3da1f
Mir Md. Asif Hossain <asif.hossain@therapservices.net>
2015-02-23 16:31:33 +0600

    Lavel 5 challenge 1

--- js/App.js
+++ js/App.js
@@ -1,62 +1,9 @@
 
 (function () {
 
-    var app = angular.module('store',[]);
+    var app = angular.module('store',['store-directives']);
     app.controller('StoreController', function () {
         this.products = gems;
-    });
-
-    app.directive('productHeading',function(){
-        return {
-            templateUrl : 'product-heading.html'
-        };
-    });
-    app.directive('productImages',function () {
-        return  {
-          templateUrl : 'product-images.html'
-        };
-    });
-    app.directive('productTabs',function(){
-        return {
-            templateUrl : 'product-tabs.html'
-        };
-    });
-
-    app.directive('productDescription', function () {
-        return {
-            restrict : 'E',
-            templateUrl : 'product-description.html'
-        };
-    });
-    app.directive('productSpecs', function () {
-        return {
-            restrict : 'E',
-            templateUrl : 'product-specs.html'
-        };
-    });
-
-    app.directive('productReviews',function() {
-        return {
-            restrict : 'E',
-            templateUrl : 'product-reviews.html'
-        };
-    });
-
-    app.directive('reviewForm',function(){
-       return {
-           restrict : 'E',
-           templateUrl : 'review-form.html',
-           controller : function () {
-               this.review = {};
-               this.submitReview = function (product) {
-
-                   this.review.createdOn = Date.now();
-                   product.reviews.push(this.review);
-                   this.review = {};
-               }
-           },
-           controllerAs : 'formCtrl'
-       };
     });
 
     var gems = [
