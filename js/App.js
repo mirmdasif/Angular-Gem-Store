8c0268819b1498aac6a5054cfff98f4cedf66eef
Mir Md. Asif Hossain <asif.hossain@therapservices.net>
2015-02-23 16:17:28 +0600

    Level 4 challenges completed

--- js/App.js
+++ js/App.js
@@ -6,14 +6,20 @@
         this.products = gems;
     });
 
-    app.controller('ReviewFormController', function () {
-        this.review = {};
-        this.submitReview = function (product) {
-
-            this.review.createdOn = Date.now();
-            product.reviews.push(this.review);
-            this.review = {};
-        }
+    app.directive('productHeading',function(){
+        return {
+            templateUrl : 'product-heading.html'
+        };
+    });
+    app.directive('productImages',function () {
+        return  {
+          templateUrl : 'product-images.html'
+        };
+    });
+    app.directive('productTabs',function(){
+        return {
+            templateUrl : 'product-tabs.html'
+        };
     });
 
     app.directive('productDescription', function () {
@@ -28,6 +34,31 @@
             templateUrl : 'product-specs.html'
         };
     });
+
+    app.directive('productReviews',function() {
+        return {
+            restrict : 'E',
+            templateUrl : 'product-reviews.html'
+        };
+    });
+
+    app.directive('reviewForm',function(){
+       return {
+           restrict : 'E',
+           templateUrl : 'review-form.html',
+           controller : function () {
+               this.review = {};
+               this.submitReview = function (product) {
+
+                   this.review.createdOn = Date.now();
+                   product.reviews.push(this.review);
+                   this.review = {};
+               }
+           },
+           controllerAs : 'formCtrl'
+       };
+    });
+
     var gems = [
         {
             name: 'Azurite',
